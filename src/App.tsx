cf5643a857e12249b8cd6400601447ffa8ffbd44
Lucas Previato <lucaspreviato@live.com>
2022-08-12 14:30:50 -0300

    Added react Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import {BrowserRouter} from "react-router-dom"
+
+import { AppRoutes } from "./routes";
 
 
 export const App = () => {
   return (
+    <BrowserRouter >
     <div className="App">
-    teste
+    <AppRoutes />
     </div>
+    </BrowserRouter>
   );
 }
 
