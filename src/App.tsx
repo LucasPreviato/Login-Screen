c99c11799ab63e33bb246f5d41ffaa16f91123d0
Lucas Previato <lucaspreviato@live.com>
2022-08-14 02:25:06 -0300

    Feat: added LightTheme and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@mui/material";
 import {BrowserRouter} from "react-router-dom"
 
 import { AppRoutes } from "./routes";
@@ -5,11 +6,12 @@ import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
+    <ThemeProvider theme={LightTheme}>
+
     <BrowserRouter >
-    <div className="App">
     <AppRoutes />
-    </div>
     </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom"
 
 
@@ -5,7 +6,7 @@ export const AppRoutes = () => {
 
   return (
   <Routes>
-    <Route path="/pagina-inicial" element={<p>Página incial</p>} />
+    <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">teste</Button>} />
     <Route path="*" element={<Navigate to="/pagina-inicial" />}/> 
   </Routes>
   );
