aaa91dbd3a4610d3363a594267b47c8669dc7a2a
Lucas Previato <lucaspreviato@live.com>
2022-08-15 15:39:21 -0300

    Feat: added responsive sidemenu

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { AppRoutes } from './routes'
 import { MenuLateral } from './shared/components'
-import { AppThemeProvider } from './shared/context/'
+import { AppThemeProvider, DrawerProvider } from './shared/context/'
 
 export const App = () => {
   return (
     <AppThemeProvider>
+    <DrawerProvider>
       <BrowserRouter>
         <MenuLateral>
           <AppRoutes />
         </MenuLateral>
       </BrowserRouter>
+    </DrawerProvider>
     </AppThemeProvider>
   )
 }
--- src/shared/components/menuLateral/MenuLateral.tsx
+++ src/shared/components/menuLateral/MenuLateral.tsx
@@ -1,5 +1,6 @@
-import { useTheme, Drawer } from '@mui/material'
+import { useTheme, Drawer, Avatar, Divider, List, ListItemButton, ListItemIcon, ListItemText, Icon, useMediaQuery } from '@mui/material'
 import { Box } from '@mui/system'
+import { useDrawerContext } from '../../context'
 
 interface IMenuLateralProps {
   children: React.ReactNode
@@ -7,12 +8,33 @@ interface IMenuLateralProps {
 
 export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
   const theme = useTheme()
+  const smDown = useMediaQuery(theme.breakpoints.down("sm"))
+  const {isDrawerOpen , toggleDrawerOpen} = useDrawerContext()
   return (
     <>
-      <Drawer variant="permanent">
-        <Box width={theme.spacing(28)}>teste</Box>
+      <Drawer open={isDrawerOpen} variant={smDown ? "temporary" :"persistent"} onClose={toggleDrawerOpen}>
+        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+        <Box width="100%"height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+        <Avatar 
+        sx={{height: theme.spacing(12), width: theme.spacing(12)}}
+        src="/static/images/avatar/1.jpg" />
+        </Box>
+        <Divider />
+
+        <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Página Inicial" />
+              </ListItemButton>
+            </List>
+        </Box>
+        </Box>
       </Drawer>
-      <Box height="100vh" marginLeft={theme.spacing(28)}>
+
+      <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
         {children}
       </Box>
     </>
