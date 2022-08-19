5cbe8e0045a2c5f90547e059ab6dcd79a2a7d3aa
Lucas Previato <lucaspreviato@live.com>
2022-08-19 14:57:50 -0300

    feat: axios config

---
+++ mock/README.md
@@ -0,0 +1,13 @@
+# How Started
+
+1. open the new Terminal
+2. write the command:
+```
+yarn run json-server -wd -p 3333 ./mock/database.json
+```
+or
+
+```
+npm run json-server -wd -p 3333 ./mock/database.json
+```
+
---
+++ src/shared/services/api/axiosconfig/interceptors/ErrorInterceptor.ts
@@ -0,0 +1,13 @@
+import { AxiosError } from "axios";
+
+
+export const errorInterceptor = (error: AxiosError) => {
+if (error.message ==="Network Error"){
+    return Promise.resolve(new Error("Erro de conexão"))
+};
+
+if (error.response?.status === 401){
+    // do something
+}
+    return Promise.reject(error);
+}
